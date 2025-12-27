c3864c9ee528c0c1284f1c347c69d9ee15fd99e4
Raza <hello@madratzz.net>
2025-12-27 19:57:53 +0500

    IntWithEvent Implementation

--- Assets/_Game/Core/Scripts/EventVariables/IntWithEvent.cs
+++ Assets/_Game/Core/Scripts/EventVariables/IntWithEvent.cs
@@ -1,7 +1,45 @@
-﻿namespace ProjectCore.EventVariables
+﻿using ProjectCore.Events;
+using UnityEngine;
+
+namespace ProjectCore.Variables
 {
-    public class IntWithEvent
+    [CreateAssetMenu(fileName = "v_", menuName = "ProjectCore/Variables/Int With Event")]
+    public class IntWithEvent : Int
     {
+        [SerializeField] protected GameEvent ValueChanged;
         
+        public override void ApplyChange(int amount)
+        {
+            base.ApplyChange(amount);
+            ValueChanged.Invoke();
+        }
+
+        public override void ApplyChange(Int amount)
+        {
+            base.ApplyChange(amount);
+            ValueChanged.Invoke();
+        }
+
+        public override void SetValue(int value)
+        {
+            base.SetValue(value);
+            ValueChanged.Invoke();
+        }
+
+        public override void SetValue(Int value)
+        {
+            base.SetValue(value);
+            ValueChanged.Invoke();
+        }
+
+        public void AddListener(GameEventHandler callback)
+        {
+            ValueChanged.Handler+=callback;
+        }
+        
+        public void RemoveListener(GameEventHandler callback)
+        {
+            ValueChanged.Handler -= callback;
+        }
     }
 }

6e3ab155ce2306031b88b5a29b3749146ae9a622
Raza <hello@madratzz.net>
2025-12-27 19:57:00 +0500

    Added DataBinders with UI for Int

---
+++ Assets/_Game/UI/Binders/IntVariableBinder.cs
@@ -0,0 +1,45 @@
+﻿using ProjectCore.Events;
+using TMPro;
+using UnityEngine;
+using ProjectCore.Variables;
+
+namespace ProjectGame.Features.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class IntVariableBinder : MonoBehaviour
+    {
+        [Header("Data Source")]
+        [SerializeField] private Int DataVariable;
+        [SerializeField] private GameEvent ValueChangedEvent;
+        
+        [Header("Formatting")]
+        [SerializeField] private string Format = "{0}"; // e.g. "Score: {0}" or "Lives: {0}"
+
+        private TextMeshProUGUI _targetText;
+
+        private void Awake()
+        {
+            _targetText = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void OnEnable()
+        {
+            if (DataVariable == null) return;
+            
+            ValueChangedEvent.Handler+=(UpdateUI);
+            UpdateUI();
+        }
+
+        private void OnDisable()
+        {
+            if (DataVariable == null) return;
+            
+            ValueChangedEvent.Handler-=(UpdateUI);
+        }
+
+        private void UpdateUI()
+        {
+            _targetText.text = string.Format(Format, DataVariable.GetValue());
+        }
+    }
+}

372ef78fd9f405ba5df99c0950c920c9d5d88933
Raza <hello@madratzz.net>
2025-12-29 00:01:13 +0500

    Added FSM to VContainer Global/Root Lifetime Scope

--- Assets/_Game/Core/VContainer/RootLifetimeScope.cs
+++ Assets/_Game/Core/VContainer/RootLifetimeScope.cs
@@ -1,4 +1,7 @@
-﻿using VContainer;
+﻿using ProjectCore.StateMachine;
+using ProjectGame.Core.Interfaces;
+using UnityEngine;
+using VContainer;
 using VContainer.Unity;
 
 namespace ProjectCore
@@ -6,11 +9,15 @@ namespace ProjectCore
     // This runs automatically at game start if set up in Resources
     public class RootLifetimeScope : LifetimeScope
     {
+        [SerializeField] private FiniteStateMachine ApplicationStateMachine;
         protected override void Configure(IContainerBuilder builder)
         {
             // Register the Logic here so it's available globally
-            // Lifetime.Singleton = Global
-            builder.Register<ApplicationFlowLogic>(Lifetime.Singleton);
+            // Lifetime.Singleton = Global and Only One Instance
+            builder.Register<ApplicationFlowLogic>(Lifetime.Singleton).AsImplementedInterfaces();
+            
+            //StateMachine
+            builder.RegisterInstance(ApplicationStateMachine).AsSelf();
         }
     }
 }

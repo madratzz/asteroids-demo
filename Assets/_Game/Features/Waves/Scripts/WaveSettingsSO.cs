beaf4c7d36f98de5ecd89fb7d692c6470e873313
Raza <hello@madratzz.net>
2025-12-29 19:32:05 +0500

    WaveSettings Created

---
+++ Assets/_Game/Features/Waves/Scripts/WaveSettingsSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ProjectGame.Features.Waves
+{
+    [CreateAssetMenu(fileName = "WaveSettings", menuName = "ProjectGame/Configs/Wave Settings")]
+    public class WaveSettingsSO : ScriptableObject
+    {
+        [Header("Progression")] public int BaseAsteroidCount = 2;
+        public float SpeedIncrementPerWave = 0.5f;
+
+        [Header("Spawning")] public float SpawnBuffer = 2.0f; // Distance outside screen to spawn
+    }
+}

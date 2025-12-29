35b785fc9fbefa97acc321f67fa9faddb94c2732
Raza <hello@madratzz.net>
2025-12-29 19:34:06 +0500

    Pools Updated with IPool

--- Assets/_Game/Features/Asteroids/Scripts/AsteroidPool.cs
+++ Assets/_Game/Features/Asteroids/Scripts/AsteroidPool.cs
@@ -1,10 +1,9 @@
-﻿using ProjectGame.Core.Pooling; // Assuming you created the Generic Base
-using ProjectGame.Features.Enemies;
+﻿using ProjectGame.Core.Pooling;
 
 namespace ProjectGame.Features.Enemies
 {
     // Inherits all the Addressables and Container logic!
-    public class AsteroidPool : ObjectPoolBase<Asteroid>
+    public class AsteroidPool : ObjectPoolBase<Asteroid>, IPool<Asteroid>
     {
     }
 }
--- Assets/_Game/Features/Weapons/Scripts/ProjectilePool.cs
+++ Assets/_Game/Features/Weapons/Scripts/ProjectilePool.cs
@@ -1,9 +1,8 @@
 ﻿using ProjectGame.Core.Pooling;
-using ProjectGame.Features.Weapons.Interfaces;
 
 namespace ProjectGame.Features.Weapons
 {
-    public class ProjectilePool : ObjectPoolBase<Projectile>, IProjectilePool
+    public class ProjectilePool : ObjectPoolBase<Projectile>, IPool<Projectile>
     { 
     
     }
